29b7ede8b8a0293ec022ad65f35c80f3987eda19
Woodrow Crawford <woodrowcrawford03@gmail.com>
2020-12-11 11:10:54 -0600

    Completed overall game; trying to fix win conditions

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -14,12 +14,12 @@ void printArray()
     //system("cls") to clear screen
 }
 
-void print2dArray()
+void playTicTacToe()
 {
     bool _gameOver = false;
     char player1Name[15];
     char player2Name[15];
-    int player1Turn;
+    bool player1Turn = false;
     int player2Turn;
     int player1Choice;
     int player2Choice;
@@ -40,55 +40,136 @@ void print2dArray()
   
 
         //This sets the grid of the game
-       
-            int grid[3][3] = { {0, 0, 0 }, {0, 0, 0}, {0, 0, 0} };
+        
+        int grid[3][3] = { {0, 0, 0 }, {0, 0, 0}, {0, 0, 0} };
+            
+        if (_gameOver == false)
+        {
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     std::cout << grid[i][j];
                 }
                 std::cout << std::endl;
             }
-        
+        }
+
+
+        //Makes the players take turns until one of them wins
+        while (_gameOver == false)
+        {
             //This is player 1's turn
-            std::cout << player1Name << ", it's your turn." << "1, 2, 3, 4, 5, 6, 7, 8, 9" << std::endl;
+            std::cout << player1Name << ", it's your turn." << " 1.TopLeft, 2.TopMid, 3.TopRight, " << std::endl << "4.MidLeft, 5.Mid, 6.MidRight, " << std::endl << " 7.BotLeft, 8.BotMid, 9.BotRight";
             std::cin >> player1Choice;
             if (player1Choice == 1)
             {
                 grid[0][0] = 1;
             }
-            if (player1Choice = 2)
+            else if (player1Choice == 2)
             {
                 grid[0][1] = 1;
             }
-            if (player1Choice = 3)
+            else if (player1Choice == 3)
             {
                 grid[0][2] = 1;
             }
-            if (player1Turn = 4)
+            else if (player1Turn == 4)
             {
                 grid[1][0] = 1;
             }
-            if (player1Choice = 5)
+            else if (player1Choice == 5)
             {
                 grid[1][1] = 1;
             }
-            if (player1Choice = 6)
+            else if (player1Choice == 6)
             {
                 grid[1][2] = 1;
             }
-            if (player1Choice = 7)
+            else if (player1Choice == 7)
             {
                 grid[2][0] = 1;
             }
-            if (player1Choice = 8)
+            else if (player1Choice == 8)
             {
                 grid[2][1] = 1;
             }
-            if (player1Choice = 9)
+            else if (player1Choice == 9)
             {
                 grid[2][2] = 1;
             }
-            std::cout << grid[0][0];
+            else if (player1Choice > 9)
+            {
+                system("cls");
+                std::cout << "You went out of bounds... so your pencil broke this turn." << std::endl;
+            }
+            if (_gameOver == false)
+            {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        std::cout << grid[i][j];
+                    }
+                    std::cout << std::endl;
+                }
+            }
+           
+
+         
+            //This is player 2's turn
+            std::cout << player2Name << ", it's your turn." << " 1.TopLeft, 2.TopMid, 3.TopRight, " << std::endl << "4.MidLeft, 5.Mid, 6.MidRight, " << std::endl << " 7.BotLeft, 8.BotMid, 9.BotRight";
+            std::cin >> player2Choice;
+            if (player2Choice == 1)
+            {
+                grid[0][0] = 2;
+            }
+            else if (player2Choice == 2)
+            {
+                grid[0][1] = 2;
+            }
+            else if (player2Choice == 3)
+            {
+                grid[0][2] = 2;
+            }
+            else if (player2Choice == 4)
+            {
+                grid[1][0] = 2;
+            }
+            else if (player2Choice == 5)
+            {
+                grid[1][1] = 2;
+            }
+            else if (player2Choice == 6)
+            {
+                grid[1][2] = 2;
+            }
+            else if (player2Choice == 7)
+            {
+                grid[2][0] = 2;
+            }
+            else if (player2Choice == 8)
+            {
+                grid[2][1] = 2;
+            }
+            else if (player2Choice == 9)
+            {
+                grid[2][2] = 2;
+            }
+            else if (player2Choice > 9)
+            {
+                system("cls");
+                std::cout << "You went out of bounds... so your pencil broke this turn." << std::endl;
+            }
+            if (_gameOver == false)
+            {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        std::cout << grid[i][j];
+                    }
+                    std::cout << std::endl;
+                }
+                
+            }
+        }
+
+
 
 
 
@@ -102,7 +183,7 @@ void print2dArray()
 int main()
 {
     
-    print2dArray();
+    playTicTacToe();
     system("pause");
     
     
