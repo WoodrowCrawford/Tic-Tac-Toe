aec6dd0cf1936bad407146cc42ea6ffd3284f6ee
Woodrow Crawford <woodrowcrawford03@gmail.com>
2020-12-11 02:34:30 -0600

    Finished grid; working on player 1 turn

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -16,10 +16,15 @@ void printArray()
 
 void print2dArray()
 {
+    bool _gameOver = false;
     char player1Name[15];
     char player2Name[15];
+    int player1Turn;
+    int player2Turn;
+    int player1Choice;
+    int player2Choice;
    
-
+        //This gets the player names
         std::cout << "Welcome to Tic Tak Toe! Player 1, please input your name." << std::endl;
         std::cout << "Player 1:";
         std::cin >> player1Name;
@@ -33,21 +38,62 @@ void print2dArray()
         std::cout << "Player 2 Name: " << player2Name << std::endl;
         system("pause");
   
-        int grid[3][3] = { {0, 0, 0 }, {0, 0, 0}, {0, 0, 0} };
-        char topLeft = grid[0][0]; char topMid = grid[0][1];
 
-     
-            
-        std::cout << grid[0][0], grid[0][1], grid[0][2];
+        //This sets the grid of the game
+       
+            int grid[3][3] = { {0, 0, 0 }, {0, 0, 0}, {0, 0, 0} };
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    std::cout << grid[i][j];
+                }
+                std::cout << std::endl;
+            }
+        
+            //This is player 1's turn
+            std::cout << player1Name << ", it's your turn." << "1, 2, 3, 4, 5, 6, 7, 8, 9" << std::endl;
+            std::cin >> player1Choice;
+            if (player1Choice == 1)
+            {
+                grid[0][0] = 1;
+            }
+            if (player1Choice = 2)
+            {
+                grid[0][1] = 1;
+            }
+            if (player1Choice = 3)
+            {
+                grid[0][2] = 1;
+            }
+            if (player1Turn = 4)
+            {
+                grid[1][0] = 1;
+            }
+            if (player1Choice = 5)
+            {
+                grid[1][1] = 1;
+            }
+            if (player1Choice = 6)
+            {
+                grid[1][2] = 1;
+            }
+            if (player1Choice = 7)
+            {
+                grid[2][0] = 1;
+            }
+            if (player1Choice = 8)
+            {
+                grid[2][1] = 1;
+            }
+            if (player1Choice = 9)
+            {
+                grid[2][2] = 1;
+            }
+            std::cout << grid[0][0];
 
-        std::cout << grid[1][0], grid[1][1], grid[1][2];
-        std::cout << grid[2][0], grid[2][1], grid[2][2];
-            
+
 
         
-    
-                  
-   
+
     
 }
 
